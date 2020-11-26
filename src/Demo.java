4842b6b6b030faf1f25a44b61d01e441ecd21561
evgeny knyazkin <evgen.kudesnik@gmail.com>
2020-11-26 21:43:07 +0300

    Miles

---
+++ src/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        int TicketPrice = 49858;
+        int BonusMiles = TicketPrice/20;
+        System.out.println(BonusMiles);
+    }
+}
